af9bc87bcfc42503a9118ad634ff02acbeed42a3
iyeong-gyo <iyeong-gyo@E214>
2016-12-17 18:23:45 +0900

    kk

---
+++ Tutoring/Tutoring/TutorActing.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OracleClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace Tutoring
+{
+    public partial class TutorActing : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            OracleConnection con = new OracleConnection("Data Source=lphlox.yu.ac.kr;User ID=team204;Password=4321;");
+            OracleCommand cmd = new OracleCommand("select ID from 학생 where 학번 =2016001");
+            //OracleDataAdapter sda = new OracleDataAdapter("Select Count(*) from 사용자 where 아이디='" + TextBox1.Text + "'and 암호='" + TextBox2.Text + "'", con);
+            Response.Write(Calendar1.SelectedDate.ToShortDateString() + "< hr />");
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
