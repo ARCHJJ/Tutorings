b9016e4bcade655c1ecee48367aa7ea0e38d5467
jason1616 <jason1616@naver.com>
2016-12-18 12:04:38 +0900

    ㅁㅁㅁㅁ

--- Tutoring/Tutoring/Choice_TIme.aspx.cs
+++ Tutoring/Tutoring/Choice_TIme.aspx.cs
@@ -13,7 +13,7 @@ namespace Tutoring
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+                  
         }
 
         protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
@@ -28,6 +28,13 @@ namespace Tutoring
 
         protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (GridView1.Rows[i].Cells[6] == GridView1.Rows[i].Cells[7])
+                {
+                    ClientScript.RegisterStartupScript(typeof(Page), "alert", "<script language=javaScript>alert('신청할 수 없습니다');</script>");
+                }
+            
+           
+
             OracleConnection conn = new OracleConnection();
             conn.ConnectionString = "data source=lphlox.yu.ac.kr;user id=team204;password=4321";
             OracleCommand comm = new OracleCommand();
@@ -37,14 +44,18 @@ namespace Tutoring
             try
             {
                 conn.Open();
-                comm.CommandText = "insert into A_상담시간등록 (신청자학번, 상담장소, 시작시간, 인덱스) values (" + Session["id"] + ", :place, " + StartTime.Text + ",:Index  )";
-               // comm.CommandText = "insert into A_튜터신청현황 (강좌번호, 튜터학번, 확정여부, 인덱스, 강좌명) values (:no, " + Session["id"] + ", 0, SEQ1.nextval, :name)";
-                comm.Parameters.AddWithValue("Index", row.Cells[1].Text);
-                comm.Parameters.AddWithValue("place", row.Cells[5].Text);
+                 comm.CommandText = "insert into A_상담시간등록 (신청자학번, 상담장소, 시작시간, 인덱스) values (" + Session["id"] + ",:placeL,TO_DATE(:Sdate,'YYYY-MM-DD AM HH12:MI:SS'),:Snum )";
+                //comm.CommandText = "insert into A_상담시간등록 (신청자학번, 상담장소, 시작시간, 인덱스) values (" + Session["id"] + ",:placeL,:Sdate,:Snum )";
+                // comm.CommandText = "insert into A_튜터신청현황 (강좌번호, 튜터학번, 확정여부, 인덱스, 강좌명) values (:no, " + Session["id"] + ", 0, SEQ1.nextval, :name)";
+                comm.Parameters.AddWithValue("Snum", row.Cells[1].Text);
+                comm.Parameters.AddWithValue("Sdate", row.Cells[3].Text);
+                comm.Parameters.AddWithValue("placeL", row.Cells[5].Text);                
                 //comm.Parameters.AddWithValue("StartTime", row.Cells[3].Text);
+                //  comm.CommandText = "insert into A_상담시간등록 (신청자학번, 상담장소, 시작시간, 인덱스) values (" + Session["id"] + ","+ row.Cells[5].Text + ", " + StartTime.Text + ","+ row.Cells[1].Text + ")";
 
-
-                // Debug.WriteLine(comm.CommandText);
+                Debug.WriteLine(comm.CommandText);
+                Debug.WriteLine(row.Cells[4].Text);
+               // Debug.WriteLine(row.Cells[5].Text);
                 int odr = comm.ExecuteNonQuery();
 
                 // Response.Redirect(string.Format("home.aspx"));
